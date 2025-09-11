c1277e333ebad63a21ad1e3150b1b010da82d1e8
dkurniadi <dkurniadi@thoughtmachine.net>
2025-09-11 22:35:06 +0800

    feat(marketing): add hero-section with image and showcase page for it

--- apps/marketing/src/App.tsx
+++ apps/marketing/src/App.tsx
@@ -6,6 +6,7 @@ import { ProfileCardShowcase } from "~/pages/showcases/profile-card";
 import { TestimonialCardShowcase } from "~/pages/showcases/testimonial-card";
 import { TestimonialSectionsShowcase } from "./pages/showcases/testimonial-section";
 import { NavbarShowcase } from "./pages/showcases/navbar";
+import { HeroSectionShowcase } from "./pages/showcases/hero-section";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/showcase/navbar" element={<NavbarShowcase />} />
         <Route path="/showcase/testimonial-card" element={<TestimonialCardShowcase />} />
+        <Route path="/showcase/hero-section" element={<HeroSectionShowcase />} />
         <Route path="/showcase/testimonial-section" element={<TestimonialSectionsShowcase />} />
         <Route path="/showcase/blog-card" element={<BlogCardShowCase />} />
         <Route path="/showcase/profile-card" element={<ProfileCardShowcase />} />
---
+++ apps/marketing/src/pages/landing/sections/hero-section.tsx
@@ -0,0 +1,31 @@
+import heroImg from "~/assets/landing/hero-banner.png";
+
+export const HeroSection = () => {
+  return (
+    <section className="rounded-md bg-gray-50 px-4 py-12 md:p-16 lg:p-24">
+      <div className="flex flex-col items-center gap-12 lg:flex-row lg:gap-8">
+        <div className="flex flex-col justify-start gap-8">
+          <div className="flex flex-col justify-center gap-4 self-stretch lg:gap-6">
+            <h1 className="md:5xl flex-grow text-left text-4xl font-semibold lg:text-6xl">
+              Well crafted abstract images
+            </h1>
+            <span className="text-left text-lg font-normal text-neutral-600 md:text-xl">
+              High quality abstract images for projects, wallpaper and presentations.
+            </span>
+          </div>
+          <div className="flex w-[383px] justify-start gap-4 self-stretch">
+            <button className="w-40 cursor-pointer rounded-sm bg-white px-5 py-3 shadow-sm shadow-neutral-300 hover:bg-neutral-100">
+              Learn more
+            </button>
+            <button className="w-40 cursor-pointer rounded-sm bg-indigo-600 px-5 py-3 text-white hover:bg-indigo-800">
+              See pricing
+            </button>
+          </div>
+        </div>
+        <div className="grow rounded-lg lg:py-16">
+          <img className="object-cover" src={heroImg} alt="/" />
+        </div>
+      </div>
+    </section>
+  );
+};
