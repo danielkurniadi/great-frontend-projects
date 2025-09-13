8ffefad1c1effeb6429a49061f698b4dcfdecebc
dkurniadi <dkurniadi@thoughtmachine.net>
2025-09-13 20:15:43 +0800

    feat(marketing): add content feature-section with color unsplash image

--- apps/marketing/src/App.tsx
+++ apps/marketing/src/App.tsx
@@ -5,15 +5,21 @@ import { BlogCardShowCase } from "~/pages/showcases/blog-card";
 import { HeroFeatureSectionShowcase } from "~/pages/showcases/hero-feature-section";
 import { ProfileCardShowcase } from "~/pages/showcases/profile-card";
 import { TestimonialCardShowcase } from "~/pages/showcases/testimonial-card";
-import { TestimonialSectionsShowcase } from "./pages/showcases/testimonial-section";
-import { NavbarShowcase } from "./pages/showcases/navbar";
 import { HeroSectionShowcase } from "./pages/showcases/hero-section";
+import { TestimonialSectionsShowcase } from "~/pages/showcases/testimonial-section";
+import { NavbarShowcase } from "~/pages/showcases/navbar";
+import {
+  FeaturesColorSection,
+  FeaturesGlassSection,
+} from "~/pages/landing/sections/features-section";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/showcase/features-section" element={<FeaturesGlassSection />} />
+        <Route path="/showcase/features-section/right" element={<FeaturesColorSection />} />
         <Route path="/showcase/navbar" element={<NavbarShowcase />} />
         <Route path="/showcase/testimonial-card" element={<TestimonialCardShowcase />} />
         <Route path="/showcase/hero-section" element={<HeroSectionShowcase />} />
--- apps/marketing/src/pages/landing/sections/features-section.tsx
+++ apps/marketing/src/pages/landing/sections/features-section.tsx
@@ -1,80 +1,161 @@
 /// <reference types="vite-plugin-svgr/client" />
-// import React from "react";
 
+import ColorSplashImg from "~/assets/landing/color-unsplash.png";
+import GlassSplashImg from "~/assets/landing/glass-unsplash.png";
 import IconHd from "~/assets/icons/hd.svg?react";
 import IconDropPercent from "~/assets/icons/drop-percent.svg?react";
 import IconRainbow from "~/assets/icons/rainbow.svg?react";
+import IconRocket from "~/assets/icons/rocket.svg?react";
+import IconP2P from "~/assets/icons/p2p-line.svg?react";
+
 import { cn } from "~/lib/utils";
 
-type FeatureData = {
+export type FeaturesSectionData = {
   Icon: typeof IconHd;
   title: string;
   description: string;
 };
 
-const features: FeatureData[] = [
-  {
-    Icon: IconHd,
-    title: "5K resolution support",
-    description:
-      "All images boast a minimum resolution of 5K, ensuring crisp, crystal-clear quality.",
-  },
-  {
-    Icon: IconDropPercent,
-    title: "From water to glass",
-    description:
-      "We offer a wide array of abstractions, ranging from water to glass, and encompassing various styles including 3D and vector.",
-  },
-  {
-    Icon: IconRainbow,
-    title: "Portrait or landscape",
-    description:
-      "Effortlessly adapt your images for any platform - whether it's a stunning wallpaper or captivating Instagram reels and stories.",
-  },
-];
-
-export type FeatureSectionProps = {
-  features: FeatureData[];
+export type FeaturesContentProps = {
+  features: FeaturesSectionData[];
   splashImgSide: "left" | "right";
   splashImgSrc: string;
 };
 
-const FeaturesContent = ({ features, splashImgSide, splashImgSrc }: FeatureSectionProps) => {
+export type FeaturesSectionProps = FeaturesContentProps & {
+  sectionTitle: string;
+  sectionSubtitle: string;
+  sectionHeader: string;
+};
+
+const BaseFeaturesContent = ({
+  features,
+  splashImgSrc,
+  splashImgSide = "right",
+}: FeaturesContentProps) => {
   return (
-    <div className={cn("flex", splashImgSide === "left" ? "md: flex-row-reverse" : "flex-row")}>
-      <div className="flex flex-col">
+    <div
+      className={cn(
+        "content flex justify-between gap-12 self-stretch lg:flex-row lg:gap-8",
+        splashImgSide === "right"
+          ? "flex-col lg:flex-row"
+          : "justify-end-safe flex-col-reverse lg:flex-row-reverse",
+      )}
+    >
+      <div key="feature-ul" className="flex flex-col gap-10 self-stretch">
         {features.map((feature, idx) => {
           return (
-            <div key={`ft-li-${idx}`} className="">
-              <div className="circle h-12 w-12 items-start justify-between rounded-full">
-                {<feature.Icon className="self-center object-cover" />}
+            <div key={`feature-li-${idx}`} className="flex items-start gap-5">
+              <div className="flex h-10 w-10 flex-none items-center justify-center rounded-full bg-white shadow-sm">
+                <feature.Icon className="h-6 w-6 text-indigo-700" />
               </div>
-              <div className="flex flex-col">
-                <span>{feature.title}</span>
-                <span>{feature.description}</span>
+              <div className="flex grow flex-col justify-center gap-2 py-2.5">
+                <span className="text-lg font-semibold text-neutral-900">{feature.title}</span>
+                <span className="text-base font-normal text-neutral-600">
+                  {feature.description}
+                </span>
               </div>
             </div>
           );
         })}
       </div>
-      <div className="flex-auto">
-        <img className="object-cover" src={splashImgSrc} alt="/" />
+      <img
+        className="rounded-xl lg:aspect-video lg:max-h-[460px] lg:max-w-[640px]"
+        src={splashImgSrc}
+      />
+    </div>
+  );
+};
+
+const BaseFeaturesSection = ({
+  sectionTitle,
+  sectionSubtitle,
+  sectionHeader,
+  ...props
+}: FeaturesSectionProps) => {
+  return (
+    <div className="w-full self-stretch bg-white px-3 py-12 lg:p-24">
+      <div className="container mx-auto flex max-w-[1440px] flex-col items-center justify-center gap-12 self-stretch rounded-lg lg:gap-16">
+        <div className="header flex flex-col gap-5 self-stretch lg:px-40">
+          <div className="flex flex-col justify-center gap-3 self-stretch lg:px-10">
+            <span className="text-center text-base font-semibold text-indigo-700">
+              {sectionHeader}
+            </span>
+            <span className="text-center text-3xl font-semibold text-neutral-900 md:text-5xl">
+              {sectionTitle}
+            </span>
+          </div>
+          <span className="text-center text-lg font-normal text-neutral-600 md:text-xl">
+            {sectionSubtitle}
+          </span>
+        </div>
+
+        <BaseFeaturesContent {...props} />
       </div>
     </div>
   );
 };
 
-export const FeaturesSection = (props: FeatureSectionProps) => {
-  return (
-    <section>
-      <div className="header">
-        <h5 className="text-indigo-600">High quality images</h5>
-        <h2 className="">For designers, by designers</h2>
-        <span>
-          Unleash boundless creativity with a large repository of images optimized for designers
-        </span>
-      </div>
-      <FeaturesContent {...props} />
-    </section>
-  );
+export const FeaturesGlassSection = () => {
+  const featuresSectionProps: FeaturesSectionProps = {
+    features: [
+      {
+        Icon: IconHd,
+        title: "5K resolution support",
+        description:
+          "All images boast a minimum resolution of 5K, ensuring crisp, crystal-clear quality.",
+      },
+      {
+        Icon: IconDropPercent,
+        title: "From water to glass",
+        description:
+          "We offer a wide array of abstractions, ranging from water to glass, and encompassing various styles including 3D and vector.",
+      },
+      {
+        Icon: IconRainbow,
+        title: "Portrait or landscape",
+        description:
+          "Effortlessly adapt your images for any platform - whether it's a stunning wallpaper or captivating Instagram reels and stories.",
+      },
+    ],
+    sectionHeader: "High quality images",
+    sectionTitle: "For designers, by designers",
+    sectionSubtitle:
+      "Unleash boundless creativity with a large repository of images optimized for designers",
+    splashImgSide: "right",
+    splashImgSrc: GlassSplashImg,
+  };
+  return <BaseFeaturesSection {...featuresSectionProps} />;
+};
+
+export const FeaturesColorSection = () => {
+  const featuresSectionProps: FeaturesSectionProps = {
+    features: [
+      {
+        Icon: IconRocket,
+        title: "Faster downloads",
+        description:
+          "Our robust servers are primed to deliver the highest resolution images swiftly, ensuring a smooth download experience.",
+      },
+      {
+        Icon: IconP2P,
+        title: "Convenience for teams",
+        description:
+          "Your single account can accommodate multiple users simultaneously downloading without any disruptions, streamlining teamwork and productivity.",
+      },
+      {
+        Icon: IconRainbow,
+        title: "Royalty-free licensing",
+        description:
+          "Our straightforward, royalty-free licensing means your chosen images are yours to innovate with, without the hassle of negotiating usage rights for every new project.",
+      },
+    ],
+    sectionHeader: "Best-in class support",
+    sectionTitle: "Convenience and licensing that empowers",
+    sectionSubtitle:
+      "In a world where storytelling constantly evolves, don't let licensing and poor support hold you down.",
+    splashImgSide: "left",
+    splashImgSrc: ColorSplashImg,
+  };
+  return <BaseFeaturesSection {...featuresSectionProps} />;
 };
