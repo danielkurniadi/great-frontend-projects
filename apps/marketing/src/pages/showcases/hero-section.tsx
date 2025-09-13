130b42be43875526ebcc591a67ef1a6a0b0b57fd
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2025-09-13 20:38:19 +0800

    feat(marketing): add hero-section with image and showcase page for it (#10)
    - GFE hero-section challenge: https://www.greatfrontend.com/projects/challenges/hero-section-simple

---
+++ apps/marketing/src/pages/showcases/hero-section.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from "~/pages/landing/sections/hero-section";
+
+export const HeroSectionShowcase = () => {
+  return (
+    <section className="flex flex-col bg-gray-50">
+      <div className="mx-auto align-top">
+        <HeroSection />
+      </div>
+    </section>
+  );
+};
