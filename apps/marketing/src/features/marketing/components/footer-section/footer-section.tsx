06b44f3cca6e65fd88db3746c035dd19fd7e9f40
dkurniadi <dkurniadi@thoughtmachine.net>
2025-09-27 11:51:39 +0800

    feat(marketing): add footer section for marketing landing page

---
+++ apps/marketing/src/features/marketing/components/footer-section/footer-section.tsx
@@ -0,0 +1,75 @@
+import {
+  RiGithubLine,
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookBoxLine,
+  RiTwitterXLine,
+} from "@remixicon/react";
+
+export const FooterSection = () => {
+  const footerNavItems = [
+    {
+      id: "footer-nav-feature",
+      label: "Feature",
+      href: "#",
+    },
+    {
+      id: "footer-nav-pricing",
+      label: "Pricing",
+      href: "#",
+    },
+    {
+      id: "footer-nav-aboutus",
+      label: "About Us",
+      href: "#",
+    },
+    {
+      id: "footer-nav-contactus",
+      label: "Contact Us",
+      href: "#",
+    },
+  ];
+
+  const socialLogoItems = [
+    RiYoutubeLine,
+    RiInstagramLine,
+    RiFacebookBoxLine,
+    RiGithubLine,
+    RiTwitterXLine,
+  ];
+
+  return (
+    <div className="mx-auto flex h-full flex-col justify-center md:px-8 md:py-16">
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-center gap-2 sm:gap-4">
+          {footerNavItems.map(({ id, label, href }) => {
+            return (
+              <button key={id} type="button" className="bg-none px-1">
+                <a key={id} href={href} className="w-full cursor-pointer">
+                  <span className="text-sm font-medium text-neutral-600">{label}</span>
+                </a>
+              </button>
+            );
+          })}
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="mx-auto flex justify-center gap-6">
+            {socialLogoItems.map((Logo, idx) => {
+              return (
+                <button key={`footer-social-${idx}`} className="h-6 w-6">
+                  <Logo
+                    size={24}
+                    className="cursor-pointer items-center align-middle text-neutral-300"
+                  />
+                </button>
+              );
+            })}
+          </div>
+          <div className="mx-auto text-sm font-normal text-neutral-900">
+            &copy; 2024 Abstractly, Inc. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
